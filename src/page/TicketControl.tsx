d0c384205b10de183ed05f8de283b9fe18b65640
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-04-03 09:16:33 +0700

    Fillter ticket

--- src/page/TicketControl.tsx
+++ src/page/TicketControl.tsx
@@ -1,8 +1,8 @@
 import SearchInput from "../components/InputCommon/SearchInput";
 import Menu from "../components/Menu/Menu";
 import Navbar from "../components/Navbar/Navbar";
-import { Data_ListTicketControl } from "../Data";
-import MainLayout from "../layouts/MainLayout";
+import FillterTicket from "../features/TicketControl/FillterTicket";
+import ListTicket from "../features/TicketControl/ListTicket";
 
 const TicketControl = () => {
   return (
@@ -23,63 +23,11 @@ const TicketControl = () => {
                   <SearchInput />
                   <div></div>
                 </div>
-
-                <table className="mt-[30px] w-full pb-[54px]">
-                  <tbody id="tableManager">
-                    <tr>
-                      <th>STT</th>
-                      <th className="text-left">Số vé</th>
-                      <th className="text-left">Tên sự kiện</th>
-                      <th className="text-right">Ngày sử dụng</th>
-                      <th>Loại vé</th>
-                      <th className="text-left">Cổng check - in</th>
-                      <th></th>
-                    </tr>
-                    {Data_ListTicketControl.map((val: any, index) => {
-                      return (
-                        <tr
-                          key={index}
-                          className="text-[14px] font-medium opacity-70"
-                        >
-                          <td className="text-center">{val.id}</td>
-                          <td>{val.ticketNumber}</td>
-                          <td>{val.nameEvent}</td>
-                          <td className="text-right">{val.useDate}</td>
-                          <td className="text-center">{val.typeTicket}</td>
-                          <td>{val.checkin}</td>
-                          <td></td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
+                <ListTicket />
               </div>
             </div>
             <div className=" w-[29%] bg-[#FFFFFF] rounded-[24px] mr-[24px] mb-[32px] pb-[20px] ">
-              <div className="px-[20px]">
-                <h2 className="text-[16px] font-bold my-[25px]">Lọc vé</h2>
-                <select className="w-full text-[14px] font-semibold bg-[#F1F4F8] rounded-[8px] px-2 placeholder-shown:border-gray-500 focus:outline-none h-[40px]">
-                  <option value={0}>Hội chợ triển lãm tiêu dùng 2021</option>
-                </select>
-                <div className="flex">
-                  <h2>Tình trạng đối soát</h2>
-                  <div className="radio-btn my-[20px]">
-                    <label className="form-control">
-                      <input type="radio" name="radio" defaultChecked />
-                      Tất cả
-                    </label>
-
-                    <label className="form-control">
-                      <input type="radio" name="radio" />
-                      Đã đối soát
-                    </label>
-                    <label className="form-control">
-                      <input type="radio" name="radio" defaultChecked />
-                      Chưa đối soát
-                    </label>
-                  </div>
-                </div>
-              </div>
+              <FillterTicket />
             </div>
           </div>
         </div>
