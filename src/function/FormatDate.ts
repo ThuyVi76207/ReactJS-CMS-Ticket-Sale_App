55bdfb47381845aad034f205f56bb64a9dbe7772
thuyvy76207 <1951052237vi@ou.edu.vn>
2023-04-13 10:25:46 +0700

    handle data date

--- src/function/FormatDate.ts
+++ src/function/FormatDate.ts
@@ -2,4 +2,16 @@ const hadleConvertsSecondsToDate = (seconds: any, nanoseconds: any) => {
   return new Date(seconds * 1000 + nanoseconds / 1000000).toLocaleDateString();
 };
 
-export { hadleConvertsSecondsToDate };
+const handleConvertsSecondToTime = (seconds: any, nanoseconds: any) => {
+  return new Date(seconds * 1000 + nanoseconds / 1000000).toLocaleTimeString();
+};
+
+const reverseString = (s: string) => {
+  return s.split("").reverse().join("");
+};
+
+export {
+  hadleConvertsSecondsToDate,
+  handleConvertsSecondToTime,
+  reverseString,
+};
